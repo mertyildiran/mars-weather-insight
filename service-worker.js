e8e0c2ef057ccf73535cc8d2dea3b889e3ea4fb4
AdrianApan <mail@adrianapan.com>
2020-04-21 17:17:05 +0000

    Deploying to gh-pages from  @ b4556aa3c6637d9efd6d4935204c5e516d79d230 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5b50f8f81ff15bb6828b4c3bc7aaec87.js"
+  "/mars-weather-insight/precache-manifest.de8fd64a4705891b2afa9ae72cfee6cb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mars-weather-insight/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
