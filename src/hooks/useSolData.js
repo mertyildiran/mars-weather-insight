835100aa6f46d632eddf2d86249f53daf8f52315
Adrian <mail@adrianapan.com>
2021-01-05 12:58:30 +0000

    use optional chaining

--- src/hooks/useSolData.js
+++ src/hooks/useSolData.js
@@ -6,20 +6,20 @@ function solData(data, sol) {
       sol,
       earthDate: dateFormatter(data[sol].First_UTC),
       temp: {
-        minTemp: data[sol].AT.mn,
-        maxTemp: data[sol].AT.mx,
-        avgTemp: data[sol].AT.av // temperature in Celsius
+        minTemp: data[sol].AT?.mn,
+        maxTemp: data[sol].AT?.mx,
+        avgTemp: data[sol].AT?.av // temperature in Celsius
       },
       pres: {
-        minPres: data[sol].PRE.mn,
-        maxPres: data[sol].PRE.mx,
-        avgPres: data[sol].PRE.av // pressure in Pascal
+        minPres: data[sol].PRE?.mn,
+        maxPres: data[sol].PRE?.mx,
+        avgPres: data[sol].PRE?.av // pressure in Pascal
       },
       wind: {
-        minWind: data[sol].HWS.mn,
-        maxWind: data[sol].HWS.mx,
-        avgWind: data[sol].HWS.av, // wind swpeed in meters/second
-        dirWind: data[sol].WD.most_common.compass_point // most common
+        minWind: data[sol].HWS?.mn,
+        maxWind: data[sol].HWS?.mx,
+        avgWind: data[sol].HWS?.av, // wind swpeed in meters/second
+        dirWind: data[sol].WD?.most_common?.compass_point // most common
       },
       message: ""
     }
